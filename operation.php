0a42d2c25f1001e580b033e8cccfd719a7e30463
NinoLS <belicnino77@gmail.com>
2022-04-27 18:27:21 +0200

    Read ok

--- operation.php
+++ operation.php
@@ -1,11 +1,38 @@
 <?php 
 
-$action = $_GET["action"] ?? "read";
-$entity = $_GET["entity"] ?? "";
+require_once("database.php");
+require_once("security.php");
+
+$action = secureInput($_GET["action"] ?? "read");
+$entity = secureInput($_GET["entity"] ?? null);
+$constraints = secureInput($_GET["constraints"] ?? null);
+$columns = secureInput($_GET["columns"] ?? null);
+$columns_array = explode(",", $columns);
 
 switch ($action) {
     case 'read':
-        
+        if(!empty($entity) && !empty($columns)){
+            $request = "SELECT $columns FROM $entity";
+            $request .= empty($constraints)?"":" WHERE $constraints";
+            $result = mysqli_query($db, $request);
+
+            if($result === false){
+                echo json_encode([  
+                    "status" => REQUEST_ERROR,
+                    "message" => "Erreur requête"
+                ]);
+            } else {
+                echo json_encode([
+                    "status" => REQUEST_SUCCESS,
+                    "data" => entityToJson($entity, $columns_array, mysqli_fetch_all($result, MYSQLI_ASSOC))
+                ]);
+            }
+        } else {
+            echo json_encode([
+                "status" => REQUEST_ERROR,
+                "message" => "Impossible de lire les données"
+            ]);
+        }
         break;
     
     default:
--- security.php
+++ security.php
@@ -3,6 +3,35 @@
 define("DB_SERVER", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
-define("DB_NAME", "projet_android");
-define("DB_PORT", 3306)
+define("DB_NAME", "projet_java");
+define("DB_PORT", 3306);
+
+define("REQUEST_SUCCESS", 0);
+define("REQUEST_ERROR", 1);
+
+function secureInput($input){
+    global $db;
+    return trim(htmlspecialchars(mysqli_real_escape_string($db, $input)));
+}
+
+function entityToJson($entity, $columns, $data){
+    switch ($entity) {
+        case 'livre':
+            $json_entities = [];
+            foreach ($data as $e) {
+                $tmp = [];
+                foreach ($columns as $col) {
+                    $tmp[$col] = $e[$col];
+                }
+                $json_entities[] = $tmp;
+            }
+            return json_encode($json_entities);
+            break;
+        
+        default:
+            # code...
+            break;
+    }
+}
+
 ?>
